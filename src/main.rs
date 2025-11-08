54ef4371c4793045da50129965639d05f23762a9
zephyCode <utkarsh.pundeer2001@outlook.com>
2025-11-08 19:20:40 +0530

    user implemented

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use actix_web::{App, HttpServer, Responder, get, web};
+use std::env;
+use dotenvy::dotenv;
+use sqlx::mysql::MySqlPoolOptions;
+
+mod user;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    "Hello from rust backend!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("Database URL must be set!!");
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to the database!");
+
+    println!("Connected to database successfully.");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .configure(user::init)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
---
+++ src/user.rs
@@ -0,0 +1,133 @@
+use actix_web::{post, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use sqlx::{MySqlPool};
+
+#[derive(Deserialize, Serialize)]
+struct NewUser {
+    name: String,
+    email: String,
+    password: String
+}
+
+#[derive(Deserialize, Serialize)]
+struct ExistingUser {
+    email: String,
+    password: String
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    message: String
+}
+
+pub async fn get_all_users(pool: &MySqlPool) -> Result<Vec<String>, sqlx::Error> {
+    let rows = sqlx::query!("SELECT email FROM Users")
+        .fetch_all(pool)
+        .await?;
+
+    let users = rows
+        .into_iter()
+        .map(|row| row.email.unwrap_or_default())
+        .collect();
+
+    Ok(users)
+}
+
+
+#[post("/signup")]
+async fn sign_up(
+    user: web::Json<NewUser>, 
+    pool: web::Data<MySqlPool>
+) -> impl Responder {
+
+    match get_all_users(pool.get_ref()).await {
+        Ok(all_users) => {
+            for existing_email in all_users {
+                if existing_email == user.email {
+                    return HttpResponse::BadRequest().json(ErrorResponse {
+                        message: "User with this email already exists!".to_string(),
+                    });
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to fetch users: {:?}", e);
+            return HttpResponse::InternalServerError().json(ErrorResponse {
+                message: "Failed to check existing users.".to_string(),
+            });
+        }
+    }
+
+    if user.password.trim().len() < 8 {
+        return 
+        HttpResponse::BadRequest()
+            .json(ErrorResponse {
+                message: "Password cannot be less than 8 characters!".to_string()
+            });
+    }
+
+    let result = sqlx::query!(
+        "INSERT INTO Users (name, email, password) VALUES (?, ?, ?)",
+        user.name,
+        user.email,
+        user.password
+    )
+    .execute(pool.get_ref())
+    .await;
+
+    match result {
+        Ok(_) => HttpResponse::Ok().body("User added successfully!"),
+        Err(e) => {
+            eprintln!("DB error: {:?}", e);
+            HttpResponse::InternalServerError().body("Failed to add user!")
+        }
+    }
+}
+
+#[post("/login")]
+async fn login(
+    user: web::Json<ExistingUser>,
+    pool: web::Data<MySqlPool>
+) -> impl Responder {
+    let row = sqlx::query!(
+        "Select * from Users where email = ?",
+        user.email
+    )
+    .fetch_optional(pool.get_ref())
+    .await;
+
+    match row {
+        Ok(Some(record)) => {
+
+            let stored_email = record.email.unwrap_or_default();
+            let stored_password = record.password.unwrap_or_default();
+
+            if stored_email == user.email && stored_password == user.password {
+                HttpResponse::Ok().json(serde_json::json!({
+                    "message": "Logged in successfully."
+                }))
+            }
+            else {
+                HttpResponse::Unauthorized().json(ErrorResponse {
+                    message: "Incorrect passsword!".to_string(),
+                })
+            }
+        }
+        Ok(None) => {
+            HttpResponse::NotFound().json(ErrorResponse {
+                message: "No user found!".to_string(),
+            })
+        }
+        Err(e) => {
+            eprintln!("Database error: {:?}", e);
+            HttpResponse::InternalServerError().json(ErrorResponse {
+                message: "Database error occured!".to_string(),
+            })
+        }
+    }
+}
+
+pub fn init(cfg: &mut web::ServiceConfig) {
+    cfg.service(sign_up);
+    cfg.service(login);
+}
